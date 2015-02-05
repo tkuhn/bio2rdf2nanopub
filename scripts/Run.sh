d98935b00d8950cf26e35c1e885c8b36191baa02
Tobias Kuhn <kuhntobias@gmail.com>
2015-02-05 15:57:04 -0800

    Basic code structure

---
+++ scripts/Run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+$( dirname "${BASH_SOURCE[0]}" )/exec-class.sh ch.tkuhn.bio2rdf2nanopub.Run "$@"
---
+++ src/main/java/ch/tkuhn/bio2rdf2nanopub/Run.java
@@ -0,0 +1,9 @@
+package ch.tkuhn.bio2rdf2nanopub;
+
+public class Run {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
