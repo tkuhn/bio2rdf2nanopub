9a48bc3360903aa17cfea7356ea8efa2302f3eea
Tobias Kuhn <kuhntobias@gmail.com>
2015-02-06 15:01:23 -0800

    Use slf4j and JCommander

--- src/main/java/ch/tkuhn/bio2rdf2nanopub/Run.java
+++ src/main/java/ch/tkuhn/bio2rdf2nanopub/Run.java
@@ -1,6 +1,8 @@
 package ch.tkuhn.bio2rdf2nanopub;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import org.nanopub.Nanopub;
@@ -14,27 +16,57 @@ import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.repository.sail.SailRepositoryConnection;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.sail.memory.MemoryStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 
 public class Run {
 
-	public static void main(String[] args) {
+	@Parameter(description = "datasets", required = true)
+	private List<String> datasets = new ArrayList<String>();
+
+	public static final void main(String[] args) {
+		Run obj = new Run();
+		JCommander jc = new JCommander(obj);
 		try {
-			test();
-		} catch (Exception ex) {
-			ex.printStackTrace();
+			jc.parse(args);
+		} catch (ParameterException ex) {
+			jc.usage();
 			System.exit(1);
 		}
-		System.out.println("success");
+		obj.run();
 		System.exit(0);
 	}
 
-	private static void test() throws Exception {
+	private static Logger logger = LoggerFactory.getLogger(Run.class);
+
+	private SailRepository sailRepo;
+
+	public void run() {
+		try {
+			init();
+			for (String dataset : datasets) {
+				test(dataset);
+			}
+		} catch (Throwable th) {
+			logger.error(th.getMessage(), th);
+			System.exit(1);
+		}
+		logger.info("success");
+	}
+
+	private void init() throws Exception {
 		MemoryStore store = new MemoryStore();
 		store.initialize();
-		SailRepository sailRepo = new SailRepository(store);
+		sailRepo = new SailRepository(store);
+	}
+
+	private void test(String dataset) throws Exception {
 		SailRepositoryConnection conn = sailRepo.getConnection();
-		File file = new File(Run.class.getClassLoader().getResource("drugbank.sparql").getFile());
-		Scanner scanner = new Scanner(file);
+		Scanner scanner = new Scanner(getQueryFile(dataset));
 		String query = "";
 		while (scanner.hasNextLine()) {
 			query += scanner.nextLine() + "\n";
@@ -44,8 +76,13 @@ public class Run {
 		update.execute();
 		Nanopub nanopub = new NanopubImpl(sailRepo, new URIImpl("http://bio2rdf.org/drugbank_resource:DB00247_BE0000650_nanopub"));
 		nanopub = MakeTrustyNanopub.transform(nanopub);
-		System.err.println(NanopubUtils.writeToString(nanopub, RDFFormat.TRIG));
-		System.err.println(sailRepo.getConnection().size());
+		logger.info(NanopubUtils.writeToString(nanopub, RDFFormat.TRIG));
+		logger.info("size: " + sailRepo.getConnection().size());
+		sailRepo.getConnection().clear();
+	}
+
+	private static File getQueryFile(String dataset) {
+		return new File(Run.class.getClassLoader().getResource("queries/" + dataset + ".sparql").getFile());
 	}
 
 }
